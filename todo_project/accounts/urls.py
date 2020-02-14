e5662495d80f0228e6419770976ad50feac2c43e
Musyimi97 <collinsmusyimi.cm@gmail.com>
2020-02-14 12:17:35 +0300

    finished up with perms

--- todo_project/accounts/urls.py
+++ todo_project/accounts/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views import UserProfileCreateView, UserProfileDetailView
 
 urlpatterns = [
-    
+     #gets all user profiles and create a new profile
+    path("all-profiles",UserProfileCreateView.as_view(),name="all-profiles"),
+   # retrieves profile details of the currently logged in user
+    path("profile/<int:pk>",UserProfileDetailView.as_view(),name="profile"),
 ]
--- todo_project/accounts/views.py
+++ todo_project/accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.generics import (ListCreateAPIView,RetrieveUpdateDestroyAPIView,)
+from rest_framework.permissions import IsAuthenticated
+from .models import UserProfile
+from .permissions import IsOwnerProfileReadOnly
+from .serializers import UserProfileSerializer
 
 # Create your views here.
+class UserProfileCreateView(ListCreateAPIView):
+    queryset = UserProfile
+    serializer_class=UserProfileSerializer
+    permission_classes=[IsAuthenticated]
+
+    def perform_create(self, serializer):
+        user=self.request.user
+        serializer.save(user=user)
+
+class UserProfileDetailView(RetrieveUpdateDestroyAPIView):
+    queryset=UserProfile.objects.all()
+    serializer_class=UserProfileSerializer
+    permission_classes=[IsOwnerProfileReadOnly, IsAuthenticated]
